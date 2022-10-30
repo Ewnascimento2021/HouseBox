b55f43ca5505fedbce4dc0647f52c07cf1ff38ba
Ewnascimento <ewerton_snascimento@outlook.com>
2022-10-30 11:13:06 -0300

    Button IGiveUP

---
+++ Assets/Scripts/IGiveUpController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IGiveUpController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button IGiveUpButton;
+
+
+    private void Awake()
+    {
+        Time.timeScale = 1;
+        IGiveUpButton.onClick.AddListener(ExitToMenu);
+    }
+    private void ExitToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
