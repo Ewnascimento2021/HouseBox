b23ec7ff006000344a450beb58703203d34123c5
Vladmir C Souza <vladmir.csouza@gmail.com>
2022-11-19 23:43:31 +0000

    Setting the box variable to private and encapsulating it to use as a property.

--- Assets/Scripts/Box/CheckBox.cs
+++ Assets/Scripts/Box/CheckBox.cs
@@ -8,18 +8,17 @@ namespace Ewerton.Housebox.Box
 {
     public class CheckBox : MonoBehaviour
     {
-
-        public Sprite[] box = new Sprite[2];
-        public SpriteRenderer spriteConcha;
+        private Sprite[] box = new Sprite[2];
+        public Sprite[] Box => box;
+                
         private Rigidbody2D rb;
         public bool checkBox;
-
 
         void Start()
         {
             //spriteConcha.sprite = box[0];
             rb = GetComponent<Rigidbody2D>();
-            rb.GetComponent<SpriteRenderer>().sprite = box[0];
+            rb.GetComponent<SpriteRenderer>().sprite = Box[0];
         }
 
 
@@ -34,7 +33,7 @@ namespace Ewerton.Housebox.Box
         {
             if (other.CompareTag("Base"))
             {
-                rb.GetComponent<SpriteRenderer>().sprite = box[1];
+                rb.GetComponent<SpriteRenderer>().sprite = Box[1];
 
                 checkBox = true;
 
@@ -45,7 +44,7 @@ namespace Ewerton.Housebox.Box
         {
             if (other.CompareTag("Base"))
             {
-                rb.GetComponent<SpriteRenderer>().sprite = box[0];
+                rb.GetComponent<SpriteRenderer>().sprite = Box[0];
 
                 checkBox = false;
 
