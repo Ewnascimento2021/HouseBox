f1d833f99d3eaac86562335e81e632c69caf0f8f
Vladmir C Souza <vladmir.csouza@gmail.com>
2022-11-20 00:18:30 +0000

    Add some comments as a sugestions for improving the code

--- Assets/Scripts/GameCore/GameController.cs
+++ Assets/Scripts/GameCore/GameController.cs
@@ -1,13 +1,15 @@
 using Ewerton.Housebox.Box;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Ewerton.Housebox.GameCore
 {
     public class GameController : MonoBehaviour
-
     {
+        // TODO: Converter para property ou deixar como private já que vc faz o FindGameObjectsWithTag no Start
+        // Se já sabe os objetos eu recomendo já adicionar no array via inspector, se não é OK deixar assim.
+        // Também notei que você faz um GetComponent, recomendo fazer um array de CheckBox:
+        // public CheckBox[] quantBox;
+
         public GameObject[] quantBox;
         private int boxCount;
 
@@ -22,6 +24,23 @@ namespace Ewerton.Housebox.GameCore
 
         void Update()
         {
+            // TODO: Acho que dá para deixar esse loop um pouco mais fácil de ler e trocar a variavel boxCount pelo i do for.
+            // Por exemplo:
+            /*
+            for (int i = 0; i < quantBox.Length; i++)
+            {
+                if (quantBox[i].GetComponent<CheckBox>().IsBoxChecked)
+                {
+                    continue;
+                }
+
+                if (i == quantBox.Length)
+                {
+                    //TODO: Chamar Proxima Fase;
+                }
+            }
+            */
+
             boxCount = 0;
 
             for (int i = 0; i < quantBox.Length; i++)
